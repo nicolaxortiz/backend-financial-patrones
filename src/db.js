38e8915c64ccd9f6edb3fb8f678122fc84ca5bd9
nicolaxortiz <nicolaxortiz@gmail.com>
2025-04-17 10:32:27 -0500

    vercel configuration

--- src/db.js
+++ src/db.js
@@ -1,13 +1,5 @@
 import pg from "pg";
 
-export const pool = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE_NAME,
-  port: process.env.DB_PORT,
-});
-
 export class DatabaseConnection {
   static #instance = null;
 
