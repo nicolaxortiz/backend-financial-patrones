dd9850bef7f9b68823d9cf0a491abc18e5b50550
nicolaxortiz <nicolaxortiz@gmail.com>
2025-05-29 11:14:43 -0500

    add strategy pattern

--- src/db.js
+++ src/db.js
@@ -21,7 +21,7 @@ export class DatabaseConnection {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE_NAME,
       port: process.env.DB_PORT,
-      // ssl: true,
+      ssl: true,
     });
 
     this.#pool.on("error", (err) => {
